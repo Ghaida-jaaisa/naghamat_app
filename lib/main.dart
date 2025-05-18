9c6371d8fe73f0cd2c389090a759c7a44d898ab2
Ghaida Ja'aysah <ghaida.20004@gmail.com>
2025-05-18 22:19:30 +0300

    upload

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,9 @@ class MusicWidget extends StatelessWidget {
   const MusicWidget({super.key});
 
   // دالة لتشغيل الصوت
-  void playSound() async {
+  void playSound(int musicNumber) async {
     final player = AudioPlayer();
-    await player.play(AssetSource('music-2.mp3'));
+    await player.play(AssetSource('music-$musicNumber.mp3'));
   }
 
   @override
@@ -25,16 +25,102 @@ class MusicWidget extends StatelessWidget {
           centerTitle: true,
         ),
         body: Center(
-          child: ElevatedButton.icon(
-            onPressed: playSound,
-            icon: const Icon(Icons.music_note),
-            label: const Text("شغّل النغمة 🎵"),
-            style: ElevatedButton.styleFrom(
-              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-              textStyle: const TextStyle(fontSize: 18),
-              backgroundColor: Colors.purple,
-              foregroundColor: Colors.white,
-            ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 10.0,
+            children:[
+              ElevatedButton.icon(
+                onPressed: (){
+                  playSound(1);
+                  },
+          icon: const Icon(Icons.music_note),
+          label: const Text("NOTE 1 🎵"),
+          style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+          textStyle: const TextStyle(fontSize: 18),
+          backgroundColor: Colors.purple,
+          foregroundColor: Colors.white,
+          ),
+          ),
+          ElevatedButton.icon(
+          onPressed: (){
+                playSound(2);
+              },
+          icon: const Icon(Icons.music_note),
+          label: const Text("NOTE 2 🎵"),
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                  textStyle: const TextStyle(fontSize: 18),
+                  backgroundColor: Colors.purple,
+                  foregroundColor: Colors.white,
+                ),
+              ),
+              ElevatedButton.icon(
+                  onPressed: (){
+                    playSound(3);
+                  } ,
+                  icon: const Icon(Icons.music_note),
+                label: const Text("NOTE 3 🎵"),
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                  textStyle: const TextStyle(fontSize: 18),
+                  backgroundColor: Colors.purple,
+                  foregroundColor: Colors.white,
+                ),
+              ),
+              ElevatedButton.icon(
+                onPressed: (){
+                  playSound(4);
+                },
+                icon: const Icon(Icons.music_note),
+                label: const Text("NOTE 4 🎵"),
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                  textStyle: const TextStyle(fontSize: 18),
+                  backgroundColor: Colors.purple,
+                  foregroundColor: Colors.white,
+                ),
+              ),
+              ElevatedButton.icon(
+                onPressed: (){
+                  playSound(5);
+                },
+                icon: const Icon(Icons.music_note),
+                label: const Text("NOTE 5 🎵"),
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                  textStyle: const TextStyle(fontSize: 18),
+                  backgroundColor: Colors.purple,
+                  foregroundColor: Colors.white,
+                ),
+              ),
+              ElevatedButton.icon(
+                onPressed: (){
+                  playSound(6);
+                },
+                icon: const Icon(Icons.music_note),
+                label: const Text("NOTE 6 🎵"),
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                  textStyle: const TextStyle(fontSize: 18),
+                  backgroundColor: Colors.purple,
+                  foregroundColor: Colors.white,
+                ),
+              ),
+              ElevatedButton.icon(
+                onPressed: (){
+                  playSound(7);
+                },
+                icon: const Icon(Icons.music_note),
+                label: const Text("NOTE 7 🎵"),
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                  textStyle: const TextStyle(fontSize: 18),
+                  backgroundColor: Colors.purple,
+                  foregroundColor: Colors.white,
+                ),
+              ),
+            ]
           ),
         ),
       ),
